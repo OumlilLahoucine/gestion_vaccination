0c61af18c17a889e1b47dd17831fac7d58176327
Oumlil Lahoucine <104671023+OumlilLahoucine@users.noreply.github.com>
2022-06-18 15:00:07 +0100

    Update initialize.php

--- initialize.php
+++ initialize.php
@@ -4,7 +4,7 @@ if(!defined('base_url')) define('base_url','http://localhost/vaccinated/');
 if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
 if(!defined('dev_data')) define('dev_data',$dev_data);
 if(!defined('DB_SERVER')) define('DB_SERVER',"localhost");
-if(!defined('DB_USERNAME')) define('DB_USERNAME',"root");
-if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"pass");
+if(!defined('DB_USERNAME')) define('DB_USERNAME',"user_cloud");
+if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"pass_cloud");
 if(!defined('DB_NAME')) define('DB_NAME',"vaccinated_db");
 ?>
